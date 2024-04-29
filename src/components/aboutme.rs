d394e70f34c65098bd9156682ab724a71c212768
Steinar Darri Þorgilsson <steinardth@gmail.com>
2024-04-29 00:07:16 +0000

    Fixed fmt issues

--- src/components/aboutme.rs
+++ src/components/aboutme.rs
@@ -57,7 +57,7 @@ pub fn AboutMe() -> Html {
         })
         .collect();
 
-    html!{
+    html! {
     <>    // About Me
           <div id="aboutme" class="flex flex-col justify-center bg-gradient-to-b from-fuchsia-300
           to-violet-300 dark:from-fuchsia-600 dark:to-violet-700 pl-12 lg:pl-44 pr-20 w-full h-full">
