cd544c77dda0df8e3361d61296891d7b78f66b1c
Steinar Darri Þorgilsson <steinardth@gmail.com>
2024-04-30 16:15:22 +0000

    Fixed format tooling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-const fs = require('fs')
+const fs = require("fs");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // configure the paths to all of your source files
-  content: { files: [
-    './**/*.{html,md,rs,yaml}',
-  ]},
+  content: { files: ["./**/*.{html,md,rs,yaml}"] },
   theme: {
     fontFamily: {
-      sans: ['Prompt', 'sans-serif'],
-      serif: ['Lustria', 'Yrsa', 'serif'],
-      mono: ['Hack', 'monospace'],
+      sans: ["Prompt", "sans-serif"],
+      serif: ["Lustria", "Yrsa", "serif"],
+      mono: ["Hack", "monospace"],
     },
     extend: {
       // extend base Tailwind CSS utility classes
@@ -19,17 +17,17 @@ module.exports = {
 
   // add plugins to your Tailwind CSS project
   plugins: [
-    require('tailwind-typewriter')({
-        wordsets: {
-            aboutme: {
-              words: [fs.readFileSync('./res/content/aboutme.md', 'utf8')],
-              writeSpeed: 0.05,
-              pauseBetween: 1,
-              repeat: 0,
-              eraseSpeed: 0,
-              caretWidth: '3px'
-            }
-        }
-    })
+    require("tailwind-typewriter")({
+      wordsets: {
+        aboutme: {
+          words: [fs.readFileSync("res/content/aboutme.md", "utf8")],
+          writeSpeed: 0.05,
+          pauseBetween: 1,
+          repeat: 0,
+          eraseSpeed: 0,
+          caretWidth: "3px",
+        },
+      },
+    }),
   ],
-}
+};
